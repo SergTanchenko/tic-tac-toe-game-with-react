b42023e6413de1cb193d8a906d900beebe9cf54c
Sergii Tanchenko <serg.tanchenko@gmail.com>
2018-07-04 23:21:14 +0300

    square components receive values from the Board component and inform the Board component when they are clicked

--- src/index.js
+++ src/index.js
@@ -3,28 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    }
-  }
 
   render() {
+    const {value, onClick} = this.props;
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: "X"})}
+        onClick={() => onClick()}
       >
-        {this.state.value}
+        {value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({squares: squares});
   }
 
   render() {
